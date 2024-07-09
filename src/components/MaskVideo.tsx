1a32b9432165ea8e5527fb88c9d5952db98ed1d9
ndltuan <ndltuan@cmcglobal.vn>
2024-07-09 19:26:32 +0700

    Update video

--- src/components/MaskVideo.tsx
+++ src/components/MaskVideo.tsx
@@ -11,10 +11,14 @@ const h = 480
 function MaskVideo(props: Props) {
   const refVideo = useRef<HTMLVideoElement | null>(null)
   const refCanvas = useRef<HTMLCanvasElement | null>(null)
-  const refVideoFinal = useRef<HTMLVideoElement | null>(null)
   const animationFrameId = useRef<number | null>(null)
 
-  const drawCanvas = () => {
+  const detectObject = (ctx: CanvasRenderingContext2D) => {
+    ctx.getImageData(0, 0, w, h)
+    gaussianBlurArea(ctx, 100, 100, 200, 200, 10)
+  }
+
+  const drawCanvas = useCallback(() => {
     if (refVideo.current && refCanvas.current) {
       const ctx = refCanvas.current.getContext("2d", {
         willReadFrequently: true,
@@ -22,10 +26,11 @@ function MaskVideo(props: Props) {
 
       if (ctx) {
         ctx.drawImage(refVideo.current, 0, 0, w, h)
-        gaussianBlurArea(ctx, 200, 200, 200, 200, 10)
+        detectObject(ctx)
       }
     }
-  }
+  }, [])
+
   const setupWebcam = useCallback(async () => {
     if (refVideo.current && props.stream) {
       refVideo.current.srcObject = props.stream
@@ -39,26 +44,35 @@ function MaskVideo(props: Props) {
     }
   }, [props.stream])
 
-  const startRecording = useCallback(() => {
+  const startStreamCanvas = useCallback(() => {
     if (!refCanvas.current) return
 
     const stream = refCanvas.current.captureStream(40) // 30 FPS
-    if (refVideoFinal.current) {
-      refVideoFinal.current.srcObject = stream
+    // create video and attribute
+    const video = document.createElement("video")
+    video.srcObject = stream
+    video.width = w
+    video.height = h
+    video.controls = true
+    video.autoplay = true
+
+    const viewer = document.getElementById("viewer")
+    if (viewer) {
+      viewer.append(video)
     }
   }, [])
 
   const drawLoop = useCallback(() => {
     drawCanvas()
     animationFrameId.current = requestAnimationFrame(drawLoop)
-  }, [])
+  }, [drawCanvas])
 
   const main = useCallback(async () => {
     await setupWebcam()
     drawCanvas()
     drawLoop()
-    startRecording()
-  }, [drawLoop, setupWebcam, startRecording])
+    startStreamCanvas()
+  }, [drawCanvas, drawLoop, setupWebcam, startStreamCanvas])
 
   useEffect(() => {
     if (refVideo.current && refCanvas.current && props.stream) {
@@ -73,23 +87,22 @@ function MaskVideo(props: Props) {
   }, [props.stream, main])
 
   return (
-    <div>
-      <video ref={refVideo} width="640" height="480" autoPlay></video>
+    <div id="viewer">
+      <video
+        id="userId-680"
+        ref={refVideo}
+        width="640"
+        height="480"
+        autoPlay
+        className="display-none"
+      ></video>
       <canvas
         ref={refCanvas}
         width="640"
         id="canvas"
         height="480"
-        // className="display-none"
+        className="display-none"
       ></canvas>
-      <video
-        className="ml-4"
-        ref={refVideoFinal}
-        width="640"
-        height="480"
-        autoPlay
-        controls
-      />
     </div>
   )
 }
